1a6d9ab2223fb431eb119a21bd14642568147f3c
Bramvang123 <70744553+Bramvang123@users.noreply.github.com>
2020-09-04 10:49:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworldconsele
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.WriteLine("wat is je naam?");
+
+            string ingevuld = Console.ReadLine();
+            Console.WriteLine($"hallo {ingevuld}");
+
+            Console.ReadLine();
+        }
+    }
+}
